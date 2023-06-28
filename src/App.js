20c87f6da0a54840ffa6d1538656f2223c9b90e4
Juan Ignacio Rosmirez <juanrosmirez@gmail.com>
2023-06-28 18:00:20 -0300

    fix: name file and app name

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-import AutocorrectTextarea from './components/AutocorrectTextArea';
+import AutocorrectTextarea from './components/AutocorrectTextarea';
 import CORRECTIONS from './constants/corrections';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Autocorrect App
+        Autocorrection App
       </header>
       <AutocorrectTextarea corrections={CORRECTIONS}/>
     </div>
